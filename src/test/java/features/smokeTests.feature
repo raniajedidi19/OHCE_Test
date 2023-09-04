79f044fe37474f284d072c7dd4b904b2d884ad42
Rania JEDIDI <rjedidi@mobelite.fr>
2023-09-04 15:19:51 +0100

    added smoke tests feature

---
+++ src/test/java/features/smokeTests.feature
@@ -0,0 +1,26 @@
+Feature: Smoke tests
+
+  The following tests verifies that the user is able to log in, that his library opens and that he is able to log out
+
+  Background:
+    Given user goes to the base url
+    When Login page opens
+    And user accept cookies
+
+  @functional
+  Scenario: Test success login with valid credentials
+    When user enters his credentials and login
+    Then user is logged in successfully to his library
+    And An Empty library is opened
+
+  @functional
+  Scenario: Test success logout
+    When user enters his credentials and login
+    Then user is logged in successfully to his library
+    When user log out
+    Then Login page opens
+
+  @functional
+  Scenario: Test failed login
+    When user enters his credentials and login
+    Then user gets an error message
---
+++ src/test/java/stepsDefinitions/steps.java
@@ -0,0 +1,52 @@
+package stepsDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class steps {
+
+
+    @Given("user goes to the base url")
+    public void user_goes_to_the_base_url() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Login page opens")
+    public void login_page_opens() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user accept cookies")
+    public void user_accept_cookies() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enters his credentials and login")
+    public void user_enters_his_credentials_and_login() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user is logged in successfully to his library")
+    public void user_is_logged_in_successfully_to_his_library() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("An Empty library is opened")
+    public void an_empty_library_is_opened() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user log out")
+    public void user_log_out() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user gets an error message")
+    public void user_gets_an_error_message(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
