ebf57d5b58ef6307fe061481be746fd02feee307
Rania JEDIDI <rjedidi@mobelite.fr>
2023-09-15 15:27:54 +0100

    Adde dependency to  pom

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
     @RunWith(Cucumber.class)
     @CucumberOptions(
-            features = "src/test/java/features",
+            features = "src/test/java/features/smokeTests.feature",
             glue = "stepsDefinitions",
             tags = "@functional",
             plugin = {"pretty",
